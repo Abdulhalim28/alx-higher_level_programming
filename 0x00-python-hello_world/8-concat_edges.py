7e2c886217e1ad8c01cdce6e25e303fe689a881f
ADEYEMI ABDULHALIM <abdulhalimadeyemi@.com>
2024-07-10 10:20:41 +1300

    8-concat_edges.py

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
- language that combines remarkable power with very clear syntax"
- # YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
+ language that combines remarkable power\
+with very clear syntax"
 print(str)
+
 
