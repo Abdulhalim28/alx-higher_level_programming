3e3a8066031fdddd0db9593272bae6601ad85221
ADEYEMI ABDULHALIM <abdulhalimadeyemi@.com>
2024-07-10 10:33:43 +1300

    8-concat_edges.py

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-str = "Python is an interpreted, interactive, object-oriented programming\
-         language that combines remarkable power with very clear syntax"
-         print(str)
+str = "Python is an interpreted, interactive, object-oriented programming\ language that combines remarkable power with very clear syntax"
+words = str.split()
+print(*words[:4], end=" ")
+print("with", end=" ")
+print(*words[5:])
 
 
