ae0089e2c29b290f9fe4a59e3d563a7216a8a0bc
ADEYEMI ABDULHALIM <abdulhalimadeyemi@.com>
2024-07-12 01:07:05 +1300

    0-positive_or_negative.py

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -1,14 +1,13 @@
-mport random
-
+#!/usr/bin/python3
+import random
 number = random.randint(-10, 10)
-
 print(number, end='')
-
 if number > 0:
         print(" is positive")
     elif number == 0:
             print(" is zero")
- else:  
-print(" is negative")
-print()
+        else:
+                print(" is negative")
+                print()
+
 
