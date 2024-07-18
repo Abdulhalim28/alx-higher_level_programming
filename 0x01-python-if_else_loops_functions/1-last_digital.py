2d63442d94a055aadde4cb3e4be241c84918541f
ADEYEMI ABDULHALIM <abdulhalimadeyemi@.com>
2024-07-19 06:22:47 +1300

    1-last_digital.py

---
+++ 0x01-python-if_else_loops_functions/1-last_digital.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+mport random
+number = random.randint(-10000, 10000); print(f"Last digit of {number} is {abs(number)%10} and is {'greater than 5' if abs(number)%10>5 else '0' if abs(number)%10==0 else 'less than 6 and not 0'}")
