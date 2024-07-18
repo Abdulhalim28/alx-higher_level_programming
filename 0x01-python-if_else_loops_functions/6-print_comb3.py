5f80cb4b6be2929eed2ed4912bbf348a353499c0
ADEYEMI ABDULHALIM <abdulhalimadeyemi@.com>
2024-07-19 08:18:39 +1300

    6-print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 for i in range(1, 10):
-        for j in range(i+1, 10):
-    print("{:02d}".format(i*10+j), end=', ')
-    print()
+ for j in range(i+1, 10):
+ print("{:02d}".format(i*10+j), end=', ')
+ print()  # This print statement should be indented to match the inner loop
+ print()  # This print statement should not be indented, to print a newline at the end
+
 
