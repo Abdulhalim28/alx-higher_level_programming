f5f2f8ff91159e05f9a2f57c80d5f3dda66b94c4
ADEYEMI ABDULHALIM <abdulhalimadeyemi@.com>
2024-07-19 22:26:13 +1300

    8-uppercase.py

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def uppercase(str):
-for c in str:
+    for c in str:
  if ord('a') <= ord(c) <= ord('z'):
- print("%c" % (ord(c) - 32), end='')
- else:
-     print("%c" % ord(c), end='')
- print()
+  print(chr(ord(c) - 32), end='')
+  else:
+ print(c, end='')
+print()
+
 
