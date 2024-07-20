8eb25b505e98952c5ff05ab739937bddf5406952
ADEYEMI ABDULHALIM <abdulhalimadeyemi@.com>
2024-07-21 10:38:15 +1300

    0-add.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-if _name_ == "_main_":
-    from add_o import add
-    a = 1
-    b = 2
-    result = add(a,b)
-    print{f"{a} + {b} = {result}"}
+from add_0 import add
+
+a = 1
+b = 2
+
+print(f"{a} + {b} = {add(a, b)}")
